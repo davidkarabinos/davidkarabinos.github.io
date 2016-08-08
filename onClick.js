31f8e683e8e999e90d29b9e225d3123d71f4daa8
davidkarabinos <davidjoseph.karabinos@capitalone.com>
2016-08-08 15:46:46 -0400

    intro screen close

--- onClick.js
+++ onClick.js
@@ -1,9 +1,14 @@
 function handleNameClick() {
   var opacity = document.getElementById("main-background-tint").style.opacity;
   if (opacity == 0) {
-    console.log("none");
     document.getElementById("main-background-tint").style.opacity = 0.6;
+    document.getElementById("background-img").style.width = '80%';
+    document.getElementById("background-img").style.marginLeft = '10%';
+    document.getElementById("background-img").style.marginTop = '70px';
   } else {
     document.getElementById("main-background-tint").style.opacity = 0;
+    document.getElementById("background-img").style.width = '100%';
+    document.getElementById("background-img").style.marginLeft = '0%';
+    document.getElementById("background-img").style.marginTop = '0px';
   }
 }
